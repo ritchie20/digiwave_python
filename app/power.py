178e3f1b960d036e3a344e9f0c85e597e2064667
ritchie20 <oscar.rubio.ma@gmail.com>
2016-05-18 11:15:56 -0400

    Radial form for reboot/shutdown already sending the proper option to views.py

--- app/power.py
+++
@@ -1,5 +0,0 @@
-#from flask.ext.wtf import Form
-#from wtforms import StringField, BooleanField
-#from wtforms.validators import DataRequired
-
-#class RaspOff():
--- app/views.py
+++ app/views.py
@@ -1,8 +1,22 @@
-from flask import render_template
+from flask import render_template, url_for, flash, redirect, request
 from app import app
+from forms import RaspiOff
 
 @app.route('/')
 @app.route('/index')
 
 def index():
     return render_template('index.html')
+
+
+@app.route('/raspioff', methods=['GET', 'POST'])
+def raspioff():
+    form = RaspiOff()
+    if form.validate_on_submit():
+        valor = form.onoff.data
+        archivo = open('texto.txt', 'w')
+        archivo.write(valor)
+        archivo.close()
+        flash('this is a flash test, text saved')
+        return redirect(url_for('index'))
+    return render_template('raspioff.html', form=form)
