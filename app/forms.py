d7e96ea964c48e81f404fea96293dbf357e5cad8
ritchie20 <oscar.rubio.ma@gmail.com>
2016-11-02 10:16:24 -0300

    volume form working

--- app/forms.py
+++ app/forms.py
@@ -14,13 +14,6 @@ class MpdVolume(Form):
     vol_norm = SelectField("Volume Normalization", choices=[('no', 'No'), ('yes', 'Yes')])
     replay_gain = SelectField("Replay Gain", choices=[('off', 'Off'), ('album', 'Album'), ('track', 'Track')])
     replaygain_preamp = StringField("Replay Gain Preamp", validators=[Length(min=0, max=3)])
-    audio_buff = SelectField("Audio Buffer (KB)", choices=[('1024', '1 MB'), ('2048', '2 MB'), ('4096', '4 MB'),
-                                                           ('6144', '6 MB'), ('8192', '8 MB'), ('12288', '12 MB'),
-                                                           ('16384', '16 MB'), ('24456', '24 MB'), ('32768', '32 MB')])
-    buff_fill = SelectField("Buffer Before Play", choices=[('10%', '10%'), ('20%', '20%'), ('30%', '30%'),
-                                                           ('40%', '40%'), ('50%', '50%'), ('60%', '60%'),
-                                                           ('70%', '70%'), ('80%', '80%'), ('90%', '90%'),
-                                                           ('100%', '100%')])
 
 
 # Form to configure system settings
