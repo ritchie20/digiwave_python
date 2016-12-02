0b317e6c5c70e47d8eb58c24693d54848c370032
ritchie20 <oscar.rubio.ma@gmail.com>
2016-12-02 09:51:13 -0300

    validations on scripts mpd_buffer and mpd_volume made, all working fine

--- app/scripts/mpd_buffer_save.py
+++ app/scripts/mpd_buffer_save.py
@@ -5,29 +5,32 @@ import sys
 
 # Function that replace old value for the new one
 def replace_param(comment, parameter, value):
-    # opening the file just as buffer
-    with open('/Users/oscar/mpd2.conf', 'r') as f:
-        lines = f.readlines()
-        line_number = -1
-        parameter_found = 0
-        for line in lines:
-            line_number += 1
-            line = line.rstrip('\n')
-            if parameter in line:
-                parameter_found += 1
-                break
-        # this line means the parameter given doesn't exist, file corruption!
-        if parameter_found == 0:
-            return parameter
-        # if 'comment' parameter it's empty, then don't save it in the file
-        if comment == '':
-            lines[line_number] = parameter + '  "' + value + '"\n'
-        # if 'comment' isn't empty, then save it with #
-        else:
-            lines[line_number] = comment + ' ' + parameter + '  "' + value + '"\n'
-    # writing on the same file
-    with open('/Users/oscar/mpd2.conf', 'w') as f:
-        f.writelines(lines)
+    try:
+        # opening the file just as buffer
+        with open('/Users/oscar/mpd2.conf', 'r') as f:
+            lines = f.readlines()
+            line_number = -1
+            parameter_found = 0
+            for line in lines:
+                line_number += 1
+                line = line.rstrip('\n')
+                if parameter in line:
+                    parameter_found += 1
+                    break
+            # this line means the parameter given doesn't exist, file corruption!
+            if parameter_found == 0:
+                return parameter
+            # if 'comment' parameter it's empty, then don't save it in the file
+            if comment == '':
+                lines[line_number] = parameter + '  "' + value + '"\n'
+            # if 'comment' isn't empty, then save it with #
+            else:
+                lines[line_number] = comment + ' ' + parameter + '  "' + value + '"\n'
+        # writing on the same file
+        with open('/Users/oscar/mpd2.conf', 'w') as f:
+            f.writelines(lines)
+    except IOError as error:
+        return error.errno
 
 
 # assigning arguments to variables
@@ -38,11 +41,13 @@ as_comment = '#'
 # calling 'replace_param' with the values picked by the user
 check_error = replace_param('', 'audio_buffer_size', audio_buff)
 if check_error == 'audio_buffer_size':
-    sys.stdout.write(corruption + '"Audio Buffer"')
+    sys.stdout.write('"Error trying to save Audio Buffer value"')
+if check_error == 2:
+    sys.stdout.write('"The configuration file does not exit"')
 
 check_error = replace_param('', 'buffer_before_play', buff_fill)
 if check_error == 'buffer_before_play':
-    sys.stdout.write(corruption + '"Buffer before play"')
+    sys.stdout.write('"Error trying to save Buffer before play value"')
 
 
 
--- app/scripts/mpd_volume_save.py
+++ app/scripts/mpd_volume_save.py
@@ -5,29 +5,32 @@ import sys
 
 # Function that replace old value for the new one
 def replace_param(comment, parameter, value):
-    # opening the file just as buffer
-    with open('/Users/oscar/mpd2.conf', 'r') as f:
-        lines = f.readlines()
-        line_number = -1
-        parameter_found = 0
-        for line in lines:
-            line_number += 1
-            line = line.rstrip('\n')
-            if parameter in line:
-                parameter_found += 1
-                break
-        # this line means the parameter given doesn't exist, file corruption!
-        if parameter_found == 0:
-            return parameter
-        # if 'comment' parameter it's empty, then don't save it in the file
-        if comment == '':
-            lines[line_number] = parameter + '  "' + value + '"\n'
-        # if 'comment' isn't empty, then save it with #
-        else:
-            lines[line_number] = comment + ' ' + parameter + '  "' + value + '"\n'
-    # writing on the same file
-    with open('/Users/oscar/mpd2.conf', 'w') as f:
-        f.writelines(lines)
+    try:
+        # opening the file just as buffer
+        with open('/Users/oscar/mpd2.conf', 'r') as f:
+            lines = f.readlines()
+            line_number = -1
+            parameter_found = 0
+            for line in lines:
+                line_number += 1
+                line = line.rstrip('\n')
+                if parameter in line:
+                    parameter_found += 1
+                    break
+            # this line means the parameter given doesn't exist, file corruption!
+            if parameter_found == 0:
+                return parameter
+            # if 'comment' parameter it's empty, then don't save it in the file
+            if comment == '':
+                lines[line_number] = parameter + '  "' + value + '"\n'
+            # if 'comment' isn't empty, then save it with #
+            else:
+                lines[line_number] = comment + ' ' + parameter + '  "' + value + '"\n'
+        # writing on the same file
+        with open('/Users/oscar/mpd2.conf', 'w') as f:
+            f.writelines(lines)
+    except IOError as error:
+        return error.errno
 
 
 # assigning arguments to variables
@@ -40,20 +43,22 @@ as_comment = '#'
 # calling 'replace_param' with the values picked by the user
 check_error = replace_param('', 'volume_normalization', vol_norm)
 if check_error == 'volume_normalization':
-    sys.stdout.write(corruption + '"Volume normalization"')
+    sys.stdout.write('"Error trying to save Volume normalization value"')
+if check_error == 2:
+    sys.stdout.write('"The configuration file does not exit"')
 
 check_error = replace_param('', 'replaygain', replay_gain)
 if check_error == 'replaygain':
-    sys.stdout.write(corruption + '"Replay gain"')
+    sys.stdout.write('"Error trying to save Replay gain value"')
 
 if replaygain_preamp == "":
     check_error = replace_param(as_comment, 'replaygain_preamp', replaygain_preamp)
     if check_error == 'replaygain_preamp':
-        sys.stdout.write(corruption + '"Replay Gain Preamp"')
+        sys.stdout.write('"Error trying to save Replay Gain Preamp value"')
 else:
     check_error = replace_param('', 'replaygain_preamp', replaygain_preamp)
     if check_error == 'replaygain_preamp':
-        sys.stdout.write(corruption + '"Replay Gain Preamp"')
+        sys.stdout.write('"Error trying to save Replay Gain Preamp value"')
 
 
 
