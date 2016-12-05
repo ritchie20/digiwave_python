caa96cfbec865c4768aa1faa910750cb02320bb5
ritchie20 <oscar.rubio.ma@gmail.com>
2016-12-05 09:36:42 -0300

    hostname saved on upmpdcli.conf with validations

--- app/scripts/hostname_save.py
+++ app/scripts/hostname_save.py
@@ -36,6 +36,32 @@ def save_hostname(user_host):
                 # writing on hostname file
             with open('/Users/oscar/hostname', 'w') as f:
                 f.writelines(lines)
+            return 3
+    except IOError as error:
+        return error.errno
+
+
+def save_upmpdcli(user_host, variable):
+    try:
+        with open('/Users/oscar/upmpdcli.conf', 'r') as f:
+            lines = f.readlines()
+            line_number = -1
+            parameter_found = 0
+            # searching for 127.0.1.1 word
+            for line in lines:
+                line_number += 1
+                line = line.rstrip('\n')
+                if variable in line:
+                    parameter_found += 1
+                    break
+            if parameter_found == 0:
+                return 1
+            # writing hostname on the proper line
+            else:
+                lines[line_number] = variable + '= ' + user_host + '\n'
+        # writing on hosts file
+        with open('/Users/oscar/upmpdcli.conf', 'w') as f:
+            f.writelines(lines)
     except IOError as error:
         return error.errno
 
@@ -49,5 +75,17 @@ check_error = save_hostname(user_host)
 if check_error == 1:
     sys.stdout.write('"Error trying to save the Hostname"')
 if check_error == 2:
-    sys.stdout.write('"The configuration file does not exit"')
+    sys.stdout.write('"Hostname or Host file does not exit"')
+if check_error == 3:
+    check_error = save_upmpdcli(user_host, 'friendlyname ')
+    if check_error == 1:
+        sys.stdout.write('"Error trying to save the Hostname"')
+    if check_error == 2:
+        sys.stdout.write('"Upmpdcli.conf file does not exit"')
+
+    check_error = save_upmpdcli(user_host, 'ohproductroom ')
+    if check_error == 1:
+        sys.stdout.write('"Error trying to save the Hostname"')
+
+
 
