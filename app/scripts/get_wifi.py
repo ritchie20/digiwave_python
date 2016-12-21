34748ba716045ad8634ef204a7596981d2e2cefa
ritchie20 <oscar.rubio.ma@gmail.com>
2016-12-21 14:29:21 -0300

    added files mpd.conf and upmpdcli.conf, wifi_save.py has the proper code now

--- app/scripts/get_wifi.py
+++ app/scripts/get_wifi.py
@@ -6,27 +6,23 @@ import sys
 
 class GetWifi(object):
 
-    #def get_wifi_networks(self):
-     #   p = subprocess.Popen(["sudo", "iwlist", "wlan0", "scan"], stdout=subprocess.PIPE)
-      #  output = subprocess.check_output(("grep", "ESSID"), stdin=p.stdout)
-       # return output
-
-
-    # !/usr/bin/python
-
-    import sys
-
     def get_wifi_networks(self):
-        wifi_array = []
-        with open('/Users/oscar/wifi_dummy.out', 'r') as f:
-            lines = f.readlines()
-            line_number = -1
-            for line in lines:
-                line_number += 1
-                line = line.rstrip('\n')
-                line = line.lstrip()
-                wifi_array.insert(line_number, line[6:])
-
+        # Searching for available networks
+        p = subprocess.Popen(["sudo", "iwlist", "wlan0", "scan"], stdout=subprocess.PIPE)
+        # Doing a GREP to filter by ESSID
+        output = subprocess.check_output(("grep", "ESSID"), stdin=p.stdout)
+        wifi_name = ""
+        line_number = -1
+        # Walking the output char by char
+        for line in output:
+            line_number += 1
+            # Removing empty spaces from output
+            if line != ' ':
+                # Saving the output to a new String
+                wifi_name += line
+        # Removing "ESSID:" from the string
+        wifi_name = wifi_name.replace("ESSID:","")
+        # Splitting the string into a list by "\n"
+        wifi_array = wifi_name.split("\n")
         return wifi_array
-
 
