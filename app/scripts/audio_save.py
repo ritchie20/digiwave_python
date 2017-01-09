1184996afd2c29f84c4a147f6105832237630322
ritchie20 <oscar.rubio.ma@gmail.com>
2017-01-09 14:52:27 -0300

    Addings comments to audio_save.py

--- app/scripts/audio_save.py
+++ app/scripts/audio_save.py
@@ -13,32 +13,31 @@ def save_config(audio_output):
             line_number = -1
             line_number2 = -1
             parameter_found = 0
-            # searching for 127.0.1.1 word
+            # if the audio is usb, all the lines needs to be a comment
             if audio_output == "usb":
                 for line in lines:
                     line_number += 1
                     line = line.rstrip('\n')
-                    # print line[:1]
                     if line[:1] != "#":
                         lines[line_number] = "#" + line + "\n"
+            # if the audio is hifiberry, all the lines are a comment except for the hifiberry line
             else:
                 for line in lines:
                     line_number += 1
                     line = line.rstrip('\n')
-                    # print line[:1]
                     if line[:1] != "#":
                         lines[line_number] = "#" + line + "\n"
                 for line in lines:
                     line_number2 += 1
+                    # if audio_output is part of the file, parameter_found is enabled
                     if audio_output in line:
                         parameter_found += 1
                         break
                 if parameter_found == 0:
                     return 1
-                    # writing hostname on the proper line
                 else:
+                    # replacing hifiberry line with the proper output
                     lines[line_number2] = 'dtoverlay=' + audio_output + '\n'
-        # writing on hosts file
         with open('/Users/oscar/config.txt', 'w') as f:
             f.writelines(lines)
     except IOError as error:
@@ -48,12 +47,12 @@ def save_config(audio_output):
 def save_asound(audio_output):
     # Capturing error if the file doesn't exist
     try:
-        # opening hosts file as a buffer
+        # opening file as a buffer
         with open('/Users/oscar/asound.conf', 'r') as f:
             lines = f.readlines()
             line_number = -1
             parameter_found = 0
-            # searching for 127.0.1.1 word
+            # if audio_output is "usb", the value must be 1
             if audio_output == "usb":
                 for line in lines:
                     line_number += 1
@@ -63,9 +62,8 @@ def save_asound(audio_output):
                         lines[line_number] = "	card 1\n"
                 if parameter_found == 0:
                     return 1
-                    # writing hostname on the proper line
-
             else:
+                # for any hifiberry output, the value must be "0"
                 for line in lines:
                     line_number += 1
                     line = line.rstrip('\n')
@@ -74,8 +72,6 @@ def save_asound(audio_output):
                         lines[line_number] = "	card 0\n"
                 if parameter_found == 0:
                     return 1
-                    # writing hostname on the proper line
-        # writing on hosts file
         with open('/Users/oscar/asound.conf', 'w') as f:
             f.writelines(lines)
     except IOError as error:
